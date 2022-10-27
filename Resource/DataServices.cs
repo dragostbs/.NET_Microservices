cb18291ff09234cb191e33bdd661512363911a40
Boscan Dragos <dragos.boscan.tbs@gmail.com>
2022-10-27 18:39:06 +0300

    Add project files.

---
+++ Resource/DataServices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Resource.Services;
+using System.ServiceModel;
+using System.ServiceProcess;
+
+
+namespace Resource
+{
+    public class DataServices : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+        public DataServices()
+        {
+            // Name the Windows Service
+            ServiceName = "DataServices";
+        }
+        public static void Main()
+        {
+            ServiceBase.Run(new DataServices());
+        }
+        // Start the Windows service.
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+            serviceHost = new ServiceHost(typeof(CustommerResource));
+            // Open the ServiceHostBase to create listeners and start
+            // listening for messages.
+            serviceHost.Open();
+        }
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+}
